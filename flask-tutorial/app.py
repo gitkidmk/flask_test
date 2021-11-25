135fb4b5a97981be9b50e5e0ceabfdaf00896aea
강민경 <minkyeong_kang@tmax.co.kr>
2021-11-25 19:00:47 +0900

    [feat] flask 기본 구조 및 REST API 생성

---
+++ flask-tutorial/app.py
@@ -0,0 +1,36 @@
+from itertools import count
+import json
+from flask import Flask, request, jsonify
+from markupsafe import escape
+app = Flask(__name__)
+
+count = 1
+
+@app.route("/")
+def intro():
+    # return default type is HTML
+    return "Hello World!!!"
+
+@app.route("/home/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}"
+
+@app.route("/home/login", methods = ['POST'])
+def login():
+    id_info = request.json.get('id')
+    pw_info = request.json.get('pw')
+    return {
+        'id_r': id_info,
+        'pw_r' : pw_info
+    }
+@app.route("/count")
+def count_click():
+    global count
+    
+    count += 1
+    
+    return f"<div>I count it as... <b>{count}</b></div>"
+    
+
+if __name__ == "__main__":
+    app.run()
